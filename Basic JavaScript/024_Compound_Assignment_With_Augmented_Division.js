a4aefafd22ea7c7cffff5d421a1507f20f95da33
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 15:43:45 +0530

    Update 024_Compound_Assignment_With_Augmented_Division.js

--- Basic JavaScript/024_Compound_Assignment_With_Augmented_Division.js
+++ Basic JavaScript/024_Compound_Assignment_With_Augmented_Division.js
@@ -1,3 +1,10 @@
 // Problem 024: Compound Assignment With Augmented Division
 
-// Write your solution here.
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
