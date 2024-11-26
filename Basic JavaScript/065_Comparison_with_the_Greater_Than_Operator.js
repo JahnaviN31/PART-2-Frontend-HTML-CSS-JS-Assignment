581955e94d4c7fc7dd8620922aa534f685230fea
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:27:57 +0530

    Update 065_Comparison_with_the_Greater_Than_Operator.js

--- Basic JavaScript/065_Comparison_with_the_Greater_Than_Operator.js
+++ Basic JavaScript/065_Comparison_with_the_Greater_Than_Operator.js
@@ -1,3 +1,15 @@
 // Problem 065: Comparison with the Greater Than Operator
 
-// Write your solution here.
+function testGreaterThan(val) {
+  if (val>100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val>10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+testGreaterThan(10);
