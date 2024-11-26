47a2fabfd1f1e1390aa58ee5350ca4dd7c54a5ae
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 22:26:56 +0530

    Update 046_Manipulate_Arrays_With_shift_Method.js

--- Basic JavaScript/046_Manipulate_Arrays_With_shift_Method.js
+++ Basic JavaScript/046_Manipulate_Arrays_With_shift_Method.js
@@ -1,3 +1,7 @@
 // Problem 046: Manipulate Arrays With shift Method
 
-// Write your solution here.
+// Setup
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.shift();
