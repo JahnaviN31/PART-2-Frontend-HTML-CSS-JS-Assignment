4ebc963348ad50a6e8f3b8f88afd9950824e734e
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:36:02 +0530

    Update 071_Introducing_Else_Statements.js

--- Basic JavaScript/071_Introducing_Else_Statements.js
+++ Basic JavaScript/071_Introducing_Else_Statements.js
@@ -1,3 +1,19 @@
 // Problem 071: Introducing Else Statements
 
-// Write your solution here.
+function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
