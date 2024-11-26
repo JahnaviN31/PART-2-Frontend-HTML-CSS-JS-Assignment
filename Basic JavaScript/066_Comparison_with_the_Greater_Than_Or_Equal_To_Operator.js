a5652b73224c613631595bee62ec16a2d61ab681
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:29:04 +0530

    Update 066_Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js

--- Basic JavaScript/066_Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
+++ Basic JavaScript/066_Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -1,3 +1,15 @@
 // Problem 066: Comparison with the Greater Than Or Equal To Operator
 
-// Write your solution here.
+function testGreaterOrEqual(val) {
+  if (val>=20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val>=10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+testGreaterOrEqual(10);
