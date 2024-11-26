f7bac05ab9072d02d4ee6887fa6efcf2c261a75d
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:19:56 +0530

    Update 061_Comparison_with_the_Strict_Equality_Operator.js

--- Basic JavaScript/061_Comparison_with_the_Strict_Equality_Operator.js
+++ Basic JavaScript/061_Comparison_with_the_Strict_Equality_Operator.js
@@ -1,3 +1,11 @@
 // Problem 061: Comparison with the Strict Equality Operator
 
-// Write your solution here.
+// Setup
+function testStrict(val) {
+  if (val === 7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+testStrict(10);
