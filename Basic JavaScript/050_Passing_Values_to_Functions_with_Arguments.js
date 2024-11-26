84ef3b5054c9dbfb2f6f4c3df8c85c66cb268c66
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 22:38:53 +0530

    Update 050_Passing_Values_to_Functions_with_Arguments.js

--- Basic JavaScript/050_Passing_Values_to_Functions_with_Arguments.js
+++ Basic JavaScript/050_Passing_Values_to_Functions_with_Arguments.js
@@ -1,3 +1,6 @@
 // Problem 050: Passing Values to Functions with Arguments
 
-// Write your solution here.
+function functionWithArgs(num1, num2) {
+  console.log(num1 + num2);
+}
+functionWithArgs(4,5);
