0c05c9d468ff05ce44cdb5b04e8de5c889643569
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:42:29 +0530

    Update 074_Chaining_If_Else_Statements.js

--- Basic JavaScript/074_Chaining_If_Else_Statements.js
+++ Basic JavaScript/074_Chaining_If_Else_Statements.js
@@ -1,3 +1,14 @@
 // Problem 074: Chaining If Else Statements
 
-// Write your solution here.
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) return "Tiny";
+  else if (num < 10) return "Small";
+  else if (num < 15) return "Medium";
+  else if (num < 20) return "Large";
+  else if(num >= 20) return "Huge";
+  else return "Change Me";
+  // Only change code above this line
+}
+
+testSize(7);
