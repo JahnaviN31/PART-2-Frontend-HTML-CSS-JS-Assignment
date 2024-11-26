079bf78e1a657c8db0a95dbd8873ca4a776b91e3
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 21:53:14 +0530

    Update 038_Word_Blanks.js

--- Basic JavaScript/038_Word_Blanks.js
+++ Basic JavaScript/038_Word_Blanks.js
@@ -1,3 +1,10 @@
 // Problem 038: Word Blanks
 
-// Write your solution here.
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
