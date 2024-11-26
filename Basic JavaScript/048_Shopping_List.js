96a575e3a80a29ab92dbc4738c9f5580b98d9355
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 22:32:37 +0530

    Update 048_Shopping_List.js

--- Basic JavaScript/048_Shopping_List.js
+++ Basic JavaScript/048_Shopping_List.js
@@ -1,3 +1,9 @@
 // Problem 048: Shopping List
 
-// Write your solution here.
+const myList = [
+                  ['Ice Cream', 4],
+                  ['Bournville', 5],
+                  ['Banana', 12],
+                  ['Watermelon', 1],
+                  ['Soaps', 5]
+];
