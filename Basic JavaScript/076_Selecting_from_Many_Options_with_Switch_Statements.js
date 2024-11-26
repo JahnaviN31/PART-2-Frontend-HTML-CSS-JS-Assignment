7e13dd494671bea874995b3f0b63a8deda708eaa
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:55:32 +0530

    Update 076_Selecting_from_Many_Options_with_Switch_Statements.js

--- Basic JavaScript/076_Selecting_from_Many_Options_with_Switch_Statements.js
+++ Basic JavaScript/076_Selecting_from_Many_Options_with_Switch_Statements.js
@@ -1,3 +1,26 @@
 // Problem 076: Selecting from Many Options with Switch Statements
 
-// Write your solution here.
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      answer = "alpha";
+      break;
+    case 2:
+      answer = "beta";
+      break;
+    case 3:
+      answer = "gamma";
+      break;
+    case 4:
+      answer = "delta";
+      break;      
+  }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
