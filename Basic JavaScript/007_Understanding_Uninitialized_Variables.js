957f1bc55e2d4d2009e9e44446bf41a80b954044
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 14:33:37 +0530

    Update 007_Understanding_Uninitialized_Variables.js

--- Basic JavaScript/007_Understanding_Uninitialized_Variables.js
+++ Basic JavaScript/007_Understanding_Uninitialized_Variables.js
@@ -1,3 +1,11 @@
 // Problem 007: Understanding Uninitialized Variables
 
-// Write your solution here.
+// Only change code below this line
+var a = 5;
+var b = 10;
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
