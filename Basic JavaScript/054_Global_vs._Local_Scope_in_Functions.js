578333eaf4131f4c40d7cbf5d632dc1d636da01c
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 22:53:25 +0530

    Update 054_Global_vs._Local_Scope_in_Functions.js

--- Basic JavaScript/054_Global_vs._Local_Scope_in_Functions.js
+++ Basic JavaScript/054_Global_vs._Local_Scope_in_Functions.js
@@ -1,3 +1,13 @@
 // Problem 054: Global vs. Local Scope in Functions
 
-// Write your solution here.
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = "sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
