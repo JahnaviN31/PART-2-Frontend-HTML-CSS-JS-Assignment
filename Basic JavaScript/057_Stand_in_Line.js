c9cc577e2746f0df0bba960e8acd1c1a8965835e
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:11:38 +0530

    Update 057_Stand_in_Line.js

--- Basic JavaScript/057_Stand_in_Line.js
+++ Basic JavaScript/057_Stand_in_Line.js
@@ -1,3 +1,16 @@
 // Problem 057: Stand in Line
 
-// Write your solution here.
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  return arr.shift();
+  // Only change code above this line
+}
+
+// Setup
+let testArr = [1, 2, 3, 4, 5];
+
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
