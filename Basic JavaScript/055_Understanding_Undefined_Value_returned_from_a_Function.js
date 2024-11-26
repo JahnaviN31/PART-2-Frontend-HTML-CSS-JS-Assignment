1f516cb3056daba8dcbe4354b0fe4803edb900d0
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:01:39 +0530

    Update 055_Understanding_Undefined_Value_returned_from_a_Function.js

--- Basic JavaScript/055_Understanding_Undefined_Value_returned_from_a_Function.js
+++ Basic JavaScript/055_Understanding_Undefined_Value_returned_from_a_Function.js
@@ -1,3 +1,19 @@
 // Problem 055: Understanding Undefined Value returned from a Function
 
-// Write your solution here.
+// Setup
+let sum = 0;
+
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum += 5;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
