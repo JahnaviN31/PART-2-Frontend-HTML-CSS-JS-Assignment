b7d5a7374e7ce87347fdb2555a4ed48f26fa073e
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:38:56 +0530

    Update 073_Logical_Order_in_If_Else_Statements.js

--- Basic JavaScript/073_Logical_Order_in_If_Else_Statements.js
+++ Basic JavaScript/073_Logical_Order_in_If_Else_Statements.js
@@ -1,3 +1,13 @@
 // Problem 073: Logical Order in If Else Statements
 
-// Write your solution here.
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
