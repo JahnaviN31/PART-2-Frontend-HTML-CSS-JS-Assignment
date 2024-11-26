85cfd060b74672ae4559b3e21fae9a3d4741793b
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:23:36 +0530

    Update 064_Comparison_with_the_Strict_Inequality_Operator.js

--- Basic JavaScript/064_Comparison_with_the_Strict_Inequality_Operator.js
+++ Basic JavaScript/064_Comparison_with_the_Strict_Inequality_Operator.js
@@ -1,3 +1,11 @@
 // Problem 064: Comparison with the Strict Inequality Operator
 
-// Write your solution here.
+// Setup
+function testStrictNotEqual(val) {
+  if (val !== 17) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+testStrictNotEqual(10);
