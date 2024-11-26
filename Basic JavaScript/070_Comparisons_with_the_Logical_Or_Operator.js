a03c3633252eabaf4aebf8b2d34d688ef0ca20ee
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:35:22 +0530

    Update 070_Comparisons_with_the_Logical_Or_Operator.js

--- Basic JavaScript/070_Comparisons_with_the_Logical_Or_Operator.js
+++ Basic JavaScript/070_Comparisons_with_the_Logical_Or_Operator.js
@@ -1,3 +1,14 @@
 // Problem 070: Comparisons with the Logical Or Operator
 
-// Write your solution here.
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val > 20 || val < 10) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+testLogicalOr(15);
