a2c70c6ed1df438707589890b648d526652d7786
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 15:49:21 +0530

    Update 031_Appending_Variables_to_Strings.js

--- Basic JavaScript/031_Appending_Variables_to_Strings.js
+++ Basic JavaScript/031_Appending_Variables_to_Strings.js
@@ -1,3 +1,6 @@
 // Problem 031: Appending Variables to Strings
 
-// Write your solution here.
+// Change code below this line
+const someAdjective = "fun";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
