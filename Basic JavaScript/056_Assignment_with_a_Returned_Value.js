0c3e5249c6bdde3c155078509bc5fb77929c1ffa
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 23:03:08 +0530

    Update 056_Assignment_with_a_Returned_Value.js

--- Basic JavaScript/056_Assignment_with_a_Returned_Value.js
+++ Basic JavaScript/056_Assignment_with_a_Returned_Value.js
@@ -1,3 +1,11 @@
 // Problem 056: Assignment with a Returned Value
 
-// Write your solution here.
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7);
