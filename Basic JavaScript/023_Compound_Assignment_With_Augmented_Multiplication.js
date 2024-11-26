6b457067fcc2341461ff6a2bf57e6ecd8ed5d2fa
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-26 15:43:02 +0530

    Update 023_Compound_Assignment_With_Augmented_Multiplication.js

--- Basic JavaScript/023_Compound_Assignment_With_Augmented_Multiplication.js
+++ Basic JavaScript/023_Compound_Assignment_With_Augmented_Multiplication.js
@@ -1,3 +1,10 @@
 // Problem 023: Compound Assignment With Augmented Multiplication
 
-// Write your solution here.
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
