5b169e3ca700f424d579bd6207077568a19b6f4f
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 07:02:59 +0530

    Update 092_Manipulating_Complex_Objects.js

--- Basic JavaScript/092_Manipulating_Complex_Objects.js
+++ Basic JavaScript/092_Manipulating_Complex_Objects.js
@@ -1,3 +1,25 @@
 // Problem 092: Manipulating Complex Objects
 
-// Write your solution here.
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Linkin LaPark",
+        "title": "Numb",
+        "release_year": 2008,
+        "formats": [
+            "CD",
+            "Cloud"
+        ],
+        "gold": true
+    }
+];
