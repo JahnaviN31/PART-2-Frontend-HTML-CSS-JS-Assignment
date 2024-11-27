586e999950c9459d3fd9795d4f9f05ae0a1c0ab3
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 07:11:45 +0530

    Update 098_Iterate_Odd_Numbers_With_a_For_Loop.js

--- Basic JavaScript/098_Iterate_Odd_Numbers_With_a_For_Loop.js
+++ Basic JavaScript/098_Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -1,3 +1,10 @@
 // Problem 098: Iterate Odd Numbers With a For Loop
 
-// Write your solution here.
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 0; i < 5; i++) {
+    myArray.push(2 * i + 1);
+}
