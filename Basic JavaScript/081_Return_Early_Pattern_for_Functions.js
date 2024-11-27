696c2ec4bb47467406fa796f41bdae95600dbfda
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 06:20:20 +0530

    Update 081_Return_Early_Pattern_for_Functions.js

--- Basic JavaScript/081_Return_Early_Pattern_for_Functions.js
+++ Basic JavaScript/081_Return_Early_Pattern_for_Functions.js
@@ -1,3 +1,13 @@
 // Problem 081: Return Early Pattern for Functions
 
-// Write your solution here.
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
+  if(a < 0 || b < 0) 
+    return;
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
