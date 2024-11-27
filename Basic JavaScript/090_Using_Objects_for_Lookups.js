21912846e5845851ae63d8718cf00e74eb233638
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 06:53:18 +0530

    Update 090_Using_Objects_for_Lookups.js

--- Basic JavaScript/090_Using_Objects_for_Lookups.js
+++ Basic JavaScript/090_Using_Objects_for_Lookups.js
@@ -1,3 +1,22 @@
 // Problem 090: Using Objects for Lookups
 
-// Write your solution here.
+// Setup
+function phoneticLookup(val) {
+  let result = "";
+
+  // Only change code below this line
+  const lookup = {
+    "": undefined,
+    "alpha": "Adams",
+    "bravo": "Boston",
+    "charlie": "Chicago",
+    "delta": "Denver",
+    "echo": "Easy",
+    "foxtrot": "Frank"
+  }
+  return lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+phoneticLookup("charlie");
