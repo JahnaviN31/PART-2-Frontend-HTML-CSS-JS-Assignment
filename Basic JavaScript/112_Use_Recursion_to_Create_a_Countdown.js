608a6df464258bd2fde2aed8346c19001b2a8bfe
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 23:15:47 +0530

    Update 112_Use_Recursion_to_Create_a_Countdown.js

--- Basic JavaScript/112_Use_Recursion_to_Create_a_Countdown.js
+++ Basic JavaScript/112_Use_Recursion_to_Create_a_Countdown.js
@@ -1,3 +1,8 @@
 // Problem 112: Use Recursion to Create a Countdown
 
-// Write your solution here.
+// Only change code below this line
+function countdown(n) {
+    return n < 1 ? [] : [n].concat(countdown(n - 1));
+}
+// Only change code above this line
+console.log(countdown(10));
