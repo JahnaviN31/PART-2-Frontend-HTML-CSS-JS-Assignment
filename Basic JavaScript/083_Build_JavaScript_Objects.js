5e70395c89ddfc6ca975a63af6bf5147f4837f0f
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 06:33:22 +0530

    Update 083_Build_JavaScript_Objects.js

--- Basic JavaScript/083_Build_JavaScript_Objects.js
+++ Basic JavaScript/083_Build_JavaScript_Objects.js
@@ -1,3 +1,10 @@
 // Problem 083: Build JavaScript Objects
 
-// Write your solution here.
+const myDog = {
+  // Only change code below this line
+    "name": "Bob",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["Marley", "John"]
+  // Only change code above this line
+};
