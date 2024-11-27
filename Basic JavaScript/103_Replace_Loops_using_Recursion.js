1e2bc8396ee00ce4560119d9a33af007fd3a79ae
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 23:05:34 +0530

    Update 103_Replace_Loops_using_Recursion.js

--- Basic JavaScript/103_Replace_Loops_using_Recursion.js
+++ Basic JavaScript/103_Replace_Loops_using_Recursion.js
@@ -1,3 +1,11 @@
 // Problem 103: Replace Loops using Recursion
 
-// Write your solution here.
+function sum(arr, n) {
+  // Only change code below this line
+  if (n <= 0) {
+      return 0;
+    } else {
+      return sum(arr, n - 1) + arr[n - 1];
+    }
+  // Only change code above this line
+}
