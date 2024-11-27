35556a3c871c9be632ea4ea857f2c76edf4f1d67
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 06:46:01 +0530

    Update 089_Delete_Properties_from_a_JavaScript_Object.js

--- Basic JavaScript/089_Delete_Properties_from_a_JavaScript_Object.js
+++ Basic JavaScript/089_Delete_Properties_from_a_JavaScript_Object.js
@@ -1,3 +1,13 @@
 // Problem 089: Delete Properties from a JavaScript Object
 
-// Write your solution here.
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+delete myDog.tails;
