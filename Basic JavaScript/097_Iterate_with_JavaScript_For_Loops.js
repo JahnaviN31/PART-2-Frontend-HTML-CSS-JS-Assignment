e2dfd063476a609f15bb536574ac2cb720aaf412
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 07:10:56 +0530

    Update 097_Iterate_with_JavaScript_For_Loops.js

--- Basic JavaScript/097_Iterate_with_JavaScript_For_Loops.js
+++ Basic JavaScript/097_Iterate_with_JavaScript_For_Loops.js
@@ -1,3 +1,10 @@
 // Problem 097: Iterate with JavaScript For Loops
 
-// Write your solution here.
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 0; i < 5; i++) {
+    myArray.push(i + 1);
+}
