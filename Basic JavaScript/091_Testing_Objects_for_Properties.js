4bd54673321597390c85c227a1045ffc774fefd0
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 06:59:21 +0530

    Update 091_Testing_Objects_for_Properties.js

--- Basic JavaScript/091_Testing_Objects_for_Properties.js
+++ Basic JavaScript/091_Testing_Objects_for_Properties.js
@@ -1,3 +1,9 @@
 // Problem 091: Testing Objects for Properties
 
-// Write your solution here.
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp))
+    return obj["checkProp"];
+  else return "Not Found";  
+  // Only change code above this line
+}
