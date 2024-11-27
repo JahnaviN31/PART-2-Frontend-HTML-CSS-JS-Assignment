b3ae12157012eb4f2e6d14a515afad63b3f767e1
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-27 06:27:52 +0530

    Update 082_Counting_Cards.js

--- Basic JavaScript/082_Counting_Cards.js
+++ Basic JavaScript/082_Counting_Cards.js
@@ -1,3 +1,22 @@
 // Problem 082: Counting Cards
 
-// Write your solution here.
+let count = 0;
+
+function cc(card) {
+    // Only change code below this line
+    if (card == 2 || card == 3 || card == 4 || card == 5 || card == 6)
+        count += 1;
+    else if (card == 10 || card == "J" || card == "Q" || card == "K" || card == "A")
+        count -= 1;
+    else if (card == 7 || card == 8 || card == 9)
+        count += 0;
+
+    let reply = "";
+    if (count > 0) reply = "Bet";
+    else reply = "Hold";
+
+    return count +" "+ reply;
+    // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
