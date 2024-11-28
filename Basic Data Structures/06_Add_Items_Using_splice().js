14d9d455e45a2b4965c64356cbd62d55c1977e25
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:20:37 +0530

    Update 06_Add_Items_Using_splice().js

--- Basic Data Structures/06_Add_Items_Using_splice().js
+++ Basic Data Structures/06_Add_Items_Using_splice().js
@@ -1,3 +1,10 @@
 // Problem 06: Add Items Using splice()
 
-// Write your solution here.
+function htmlColorNames(arr) {
+    // Only change code below this line
+    arr.splice(0,2,"DarkSalmon","BlanchedAlmond");
+    // Only change code above this line
+    return arr;
+}
+
+console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurquoise', 'FireBrick']));
