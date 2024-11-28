a6fee7472411caff7e07ca1284dd5b4e70e2d81d
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:54:37 +0530

    Update 23_Match_Whitespace.js

--- Regular Expressions/23_Match_Whitespace.js
+++ Regular Expressions/23_Match_Whitespace.js
@@ -1,3 +1,7 @@
 // Problem 23: Match Whitespace
 
-// Write your solution here.
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace);
+
+console.log(result);
