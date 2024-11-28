cb525b8afafd7fe7693343e8ace2a193a8f15350
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:33:35 +0530

    Update 22_Reuse_JavaScript_Code_Using_import.js

--- ES6/22_Reuse_JavaScript_Code_Using_import.js
+++ ES6/22_Reuse_JavaScript_Code_Using_import.js
@@ -1,3 +1,6 @@
 // Problem 22: Reuse JavaScript Code Using import
 
-// Write your solution here.
+import { uppercaseString, lowercaseString } from "./string_functions.js";
+// Only change code above this line
+uppercaseString("hello");
+lowercaseString("WORLD!");
