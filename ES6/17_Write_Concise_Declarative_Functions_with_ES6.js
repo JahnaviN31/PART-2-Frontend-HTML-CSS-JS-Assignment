ef58d8a2748b7697c18424db9f0d32086573abf6
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:03:18 +0530

    Update 17_Write_Concise_Declarative_Functions_with_ES6.js

--- ES6/17_Write_Concise_Declarative_Functions_with_ES6.js
+++ ES6/17_Write_Concise_Declarative_Functions_with_ES6.js
@@ -1,3 +1,12 @@
 // Problem 17: Write Concise Declarative Functions with ES6
 
-// Write your solution here.
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(newGear) {
+        this.gear = newGear;
+    }
+};
+// Only change code above this line
+bicycle.setGear(48);
+console.log(bicycle.gear);
