593f99d7927c5bdd02abe0370aa01f81c25e7274
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:26:48 +0530

    Update 12_Create_complex_multi-dimensional_arrays.js

--- Basic Data Structures/12_Create_complex_multi-dimensional_arrays.js
+++ Basic Data Structures/12_Create_complex_multi-dimensional_arrays.js
@@ -1,3 +1,11 @@
 // Problem 12: Create complex multi-dimensional arrays
 
-// Write your solution here.
+let myNestedArray = [
+    // Only change code below this line
+    'level 1',                  
+    ['level 2'],	            
+    [['level 3','deep']],	      
+    [[['level 4','deeper']]],  
+    [[[['level 5','deepest']]]]  
+    // Only change code above this line
+];
