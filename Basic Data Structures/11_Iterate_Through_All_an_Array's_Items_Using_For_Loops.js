2178006e9de4e8e95d51ddd1ef288d2ff6a9886e
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:25:24 +0530

    Update 11_Iterate_Through_All_an_Array's_Items_Using_For_Loops.js

--- Basic Data Structures/11_Iterate_Through_All_an_Array's_Items_Using_For_Loops.js
+++ Basic Data Structures/11_Iterate_Through_All_an_Array's_Items_Using_For_Loops.js
@@ -1,3 +1,13 @@
 // Problem 11: Iterate Through All an Array's Items Using For Loops
 
-// Write your solution here.
+function filteredArray(arr, elem) {
+    let newArr = [];
+    // change code below this line
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].indexOf(elem) == -1) {
+            newArr.push(arr[i]); 
+        }
+    }
+    return newArr;
+}
+console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
