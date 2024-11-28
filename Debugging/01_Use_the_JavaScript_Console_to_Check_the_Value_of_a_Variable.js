aafb552d9984a731e74bae30a3fca34c07d2cf05
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:06:22 +0530

    Update 01_Use_the_JavaScript_Console_to_Check_the_Value_of_a_Variable.js

--- Debugging/01_Use_the_JavaScript_Console_to_Check_the_Value_of_a_Variable.js
+++ Debugging/01_Use_the_JavaScript_Console_to_Check_the_Value_of_a_Variable.js
@@ -1,3 +1,9 @@
 // Problem 01: Use the JavaScript Console to Check the Value of a Variable
 
-// Write your solution here.
+let a = 5;
+let b = 1;
+a++;
+// Only change code below this line
+let sumAB = a + b;
+console.log(sumAB);
+console.log(a);
