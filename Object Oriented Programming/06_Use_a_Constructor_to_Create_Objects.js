37b720602f07299b2c67c11e9f1824bcdfd144d4
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 01:51:35 +0530

    Update 06_Use_a_Constructor_to_Create_Objects.js

--- Object Oriented Programming/06_Use_a_Constructor_to_Create_Objects.js
+++ Object Oriented Programming/06_Use_a_Constructor_to_Create_Objects.js
@@ -1,3 +1,10 @@
 // Problem 06: Use a Constructor to Create Objects
 
-// Write your solution here.
+function Dog() {
+    this.name = "Rupert";
+    this.color = "brown";
+    this.numLegs = 4;
+}
+// Only change code below this line
+
+let hound = new Dog();
