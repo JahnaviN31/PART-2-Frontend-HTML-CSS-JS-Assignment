309e80f9f2963df907149ae6e7daee6939aa84d7
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 01:56:52 +0530

    Update 10_Use_Prototype_Properties_to_Reduce_Duplicate_Code.js

--- Object Oriented Programming/10_Use_Prototype_Properties_to_Reduce_Duplicate_Code.js
+++ Object Oriented Programming/10_Use_Prototype_Properties_to_Reduce_Duplicate_Code.js
@@ -1,3 +1,9 @@
 // Problem 10: Use Prototype Properties to Reduce Duplicate Code
 
-// Write your solution here.
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+// Only change code above this line
+let beagle = new Dog("Snoopy");
