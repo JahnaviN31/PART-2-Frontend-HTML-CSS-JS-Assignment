1706d9ae71f673a5f04498de011464aff9ab59f9
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 01:02:42 +0530

    Update 12_Slice_and_Splice.js

--- Basic Algorithm Scripting/12_Slice_and_Splice.js
+++ Basic Algorithm Scripting/12_Slice_and_Splice.js
@@ -1,3 +1,17 @@
 // Problem 12: Slice and Splice
 
-// Write your solution here.
+function frankenSplice(arr1, arr2, n) {
+    let result = [];
+    if (arr2.length >= 1) {
+        for (let i = 0; i < n; i++) {
+            result.push(arr2[i]);
+        }
+    }
+    result = result.concat(arr1);
+    for (let i = n; i < arr2.length; i++) {
+        result.push(arr2[i]);
+    }
+    return result;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
