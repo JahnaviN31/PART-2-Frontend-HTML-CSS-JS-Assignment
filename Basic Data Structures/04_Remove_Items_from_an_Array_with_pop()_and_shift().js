5de75fb870b7c0f3f6bf5b38f4cc63624ee87f43
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:18:59 +0530

    Update 04_Remove_Items_from_an_Array_with_pop()_and_shift().js

--- Basic Data Structures/04_Remove_Items_from_an_Array_with_pop()_and_shift().js
+++ Basic Data Structures/04_Remove_Items_from_an_Array_with_pop()_and_shift().js
@@ -1,3 +1,8 @@
 // Problem 04: Remove Items from an Array with pop() and shift()
 
-// Write your solution here.
+function popShift(arr) {
+    let popped = arr.pop(); // Change this line
+    let shifted = arr.shift(); // Change this line
+    return [shifted, popped];
+}
+console.log(popShift(['challenge','is','not','complete']));
