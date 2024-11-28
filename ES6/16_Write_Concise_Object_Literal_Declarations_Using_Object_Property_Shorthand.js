20e1ccde4ce94b5bc841b4fa347dab8166e397cb
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:02:09 +0530

    Update 16_Write_Concise_Object_Literal_Declarations_Using_Object_Property_Shorthand.js

--- ES6/16_Write_Concise_Object_Literal_Declarations_Using_Object_Property_Shorthand.js
+++ ES6/16_Write_Concise_Object_Literal_Declarations_Using_Object_Property_Shorthand.js
@@ -1,3 +1,7 @@
 // Problem 16: Write Concise Object Literal Declarations Using Object Property Shorthand
 
-// Write your solution here.
+const createPerson = (name, age, gender) => {
+    // Only change code below this line
+    return {name,age,gender };
+    // Only change code above this line
+};
