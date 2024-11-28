9bc1d88f48428f4e5bb12a432f6c805029ab5d36
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:50:42 +0530

    Update 16_Match_Beginning_String_Patterns.js

--- Regular Expressions/16_Match_Beginning_String_Patterns.js
+++ Regular Expressions/16_Match_Beginning_String_Patterns.js
@@ -1,3 +1,6 @@
 // Problem 16: Match Beginning String Patterns
 
-// Write your solution here.
+let rickyAndCal = "Cal and Ricky both like racing.";
+let calRegex = /^Cal/; // Change this line
+let result = calRegex.test(rickyAndCal);
+console.log(result);
