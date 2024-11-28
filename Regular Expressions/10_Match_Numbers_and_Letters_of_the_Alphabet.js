042d14d95230dfde4ffc4b5d33251f4b97af043b
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:46:55 +0530

    Update 10_Match_Numbers_and_Letters_of_the_Alphabet.js

--- Regular Expressions/10_Match_Numbers_and_Letters_of_the_Alphabet.js
+++ Regular Expressions/10_Match_Numbers_and_Letters_of_the_Alphabet.js
@@ -1,3 +1,6 @@
 // Problem 10: Match Numbers and Letters of the Alphabet
 
-// Write your solution here.
+let quoteSample = "Blueberry 3.141592653s are delicious.";
+let myRegex = /[h-s2-6]/gi; // Change this line
+let result = quoteSample.match(myRegex); // Change this line
+console.log(result);
