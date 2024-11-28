6efec564b512365c2c9587bf6b5edd938662b73c
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:59:59 +0530

    Update 33_Remove_Whitespace_from_Start_and_End.js

--- Regular Expressions/33_Remove_Whitespace_from_Start_and_End.js
+++ Regular Expressions/33_Remove_Whitespace_from_Start_and_End.js
@@ -1,3 +1,7 @@
 // Problem 33: Remove Whitespace from Start and End
 
-// Write your solution here.
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let result = hello.replace(wsRegex, ""); // Change this line
+
+console.log(result);
