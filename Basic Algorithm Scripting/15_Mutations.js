8d0ed9ead334316ea08310790c435aee8558e8df
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 01:43:44 +0530

    Update 15_Mutations.js

--- Basic Algorithm Scripting/15_Mutations.js
+++ Basic Algorithm Scripting/15_Mutations.js
@@ -1,3 +1,12 @@
 // Problem 15: Mutations
 
-// Write your solution here.
+function mutation(arr) {
+    const str1 = arr[1].toLowerCase();
+    const str2 = arr[0].toLowerCase();
+    for (let i = 0; i < str1.length; i++) {
+        if (str2.indexOf(str1[i]) < 0) return false;
+    }
+    return true;
+}
+
+mutation(["hello", "hey"]);
