c8501983ad28d08eb28ca9e601b82616c4abb091
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 01:45:28 +0530

    Update 16_Chunky_Monkey.js

--- Basic Algorithm Scripting/16_Chunky_Monkey.js
+++ Basic Algorithm Scripting/16_Chunky_Monkey.js
@@ -1,3 +1,11 @@
 // Problem 16: Chunky Monkey
 
-// Write your solution here.
+function chunkArrayInGroups(arr, size) {
+    const result = [];
+    for (let i = 0; i < arr.length; i += size) {
+        result.push(arr.slice(i, i + size)); 
+    }
+    return result;
+}
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
