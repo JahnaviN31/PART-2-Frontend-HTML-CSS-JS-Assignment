d9f07c41d4a4dc9688ce13dba0c069bffa1957e9
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:13:45 +0530

    Update 12_Prevent_Infinite_Loops_with_a_Valid_Terminal_Condition.js

--- Debugging/12_Prevent_Infinite_Loops_with_a_Valid_Terminal_Condition.js
+++ Debugging/12_Prevent_Infinite_Loops_with_a_Valid_Terminal_Condition.js
@@ -1,3 +1,8 @@
 // Problem 12: Prevent Infinite Loops with a Valid Terminal Condition
 
-// Write your solution here.
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+        console.log("Still going!");
+    }
+}
+myFunc();
