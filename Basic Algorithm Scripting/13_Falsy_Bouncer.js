b6941b107227a650eeb11720d6641694bd84aa46
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 01:40:01 +0530

    Update 13_Falsy_Bouncer.js

--- Basic Algorithm Scripting/13_Falsy_Bouncer.js
+++ Basic Algorithm Scripting/13_Falsy_Bouncer.js
@@ -1,3 +1,10 @@
 // Problem 13: Falsy Bouncer
 
-// Write your solution here.
+function bouncer(arr) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] && newArr.push(arr[i]);
+    }
+    return newArr;
+}
+bouncer([7, "ate", "", false, 9]);
