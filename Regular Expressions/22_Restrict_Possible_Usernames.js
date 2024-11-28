3f0f893c0e3825722e832eff068dcb27fc1b1f8d
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:53:55 +0530

    Update 22_Restrict_Possible_Usernames.js

--- Regular Expressions/22_Restrict_Possible_Usernames.js
+++ Regular Expressions/22_Restrict_Possible_Usernames.js
@@ -1,3 +1,7 @@
 // Problem 22: Restrict Possible Usernames
 
-// Write your solution here.
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i; // Change this line
+let result = userCheck.test(username);
+
+console.log(result);
