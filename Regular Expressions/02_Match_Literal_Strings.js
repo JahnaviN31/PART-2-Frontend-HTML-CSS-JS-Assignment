5b551acf9325f1ac1f63e567002779e2daef58f6
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:42:05 +0530

    Update 02_Match_Literal_Strings.js

--- Regular Expressions/02_Match_Literal_Strings.js
+++ Regular Expressions/02_Match_Literal_Strings.js
@@ -1,3 +1,5 @@
 // Problem 02: Match Literal Strings
 
-// Write your solution here.
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
