02f820632d0abe7b7948ac5478ec6455d853c790
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 11:37:00 +0530

    Update 02_Mutate_an_Array_Declared_with_const.js

--- ES6/02_Mutate_an_Array_Declared_with_const.js
+++ ES6/02_Mutate_an_Array_Declared_with_const.js
@@ -1,3 +1,21 @@
 // Problem 02: Mutate an Array Declared with const
 
-// Write your solution here.
+// An array is declared as const s = [5, 7, 2]. 
+// Change the array to [2, 5, 7] using various element assignments.
+
+const s = [5, 7, 2];
+function editInPlace() {
+    // Only change code below this line
+
+    // Using s = [2, 5, 7] would be invalid
+    let first = s[0];
+    let second = s[1];
+    let third = s[2];
+    s.length = 0;
+    s[0] = third;
+    s[1] = first;
+    s[2] = second;
+    return s;
+    // Only change code above this line
+}
+console.log(editInPlace());
