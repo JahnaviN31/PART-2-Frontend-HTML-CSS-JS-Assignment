0a71de29de1ab213bf4471599ee47a92236ff6f3
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 01:58:51 +0530

    Update 12_Understand_the_Constructor_Property.js

--- Object Oriented Programming/12_Understand_the_Constructor_Property.js
+++ Object Oriented Programming/12_Understand_the_Constructor_Property.js
@@ -1,3 +1,11 @@
 // Problem 12: Understand the Constructor Property
 
-// Write your solution here.
+function Dog(name) {
+    this.name = name;
+}
+
+// Only change code below this line
+function joinDogFraternity(candidate) {
+    if (candidate.constructor === Dog) return true;
+    return false;
+}
