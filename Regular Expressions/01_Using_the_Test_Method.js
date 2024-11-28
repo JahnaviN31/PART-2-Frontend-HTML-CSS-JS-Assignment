f346905c345c821f72319fb825756900740f9a6e
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:41:22 +0530

    Update 01_Using_the_Test_Method.js

--- Regular Expressions/01_Using_the_Test_Method.js
+++ Regular Expressions/01_Using_the_Test_Method.js
@@ -1,3 +1,6 @@
 // Problem 01: Using the Test Method
 
-// Write your solution here.
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+console.log(result);
