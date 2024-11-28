114f8add263305ba223202669fa70ceb4d9289fa
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:11:38 +0530

    Update 09_Catch_Arguments_Passed_in_the_Wrong_Order_When_Calling_a_Function.js

--- Debugging/09_Catch_Arguments_Passed_in_the_Wrong_Order_When_Calling_a_Function.js
+++ Debugging/09_Catch_Arguments_Passed_in_the_Wrong_Order_When_Calling_a_Function.js
@@ -1,3 +1,9 @@
 // Problem 09: Catch Arguments Passed in the Wrong Order When Calling a Function
 
-// Write your solution here.
+function raiseToPower(b, e) {
+    return Math.pow(b, e);
+}
+let base = 2;
+let exp = 3;
+let power = raiseToPower(base, exp);
+console.log(power);
