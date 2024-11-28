20542057830b5faf01783f2ba164ef5fcc13c5aa
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:22:56 +0530

    Update 09_Combine_Arrays_with_the_Spread_Operator.js

--- Basic Data Structures/09_Combine_Arrays_with_the_Spread_Operator.js
+++ Basic Data Structures/09_Combine_Arrays_with_the_Spread_Operator.js
@@ -1,3 +1,9 @@
 // Problem 09: Combine Arrays with the Spread Operator
 
-// Write your solution here.
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning',...fragment,'is','fun']; // Change this line
+    return sentence;
+}
+
+console.log(spreadOut());
