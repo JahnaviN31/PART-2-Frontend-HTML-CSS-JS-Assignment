0b2d056f3604dc122fe1fb1ad70a9545a4363ffc
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 11:57:42 +0530

    Update 13_Destructuring_via_rest_elements.js

--- ES6/13_Destructuring_via_rest_elements.js
+++ ES6/13_Destructuring_via_rest_elements.js
@@ -1,3 +1,13 @@
 // Problem 13: Destructuring via rest elements
 
-// Write your solution here.
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [, , ...shorterList] = list; // Change this line
+    // Only change code above this line
+    return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+
+console.log(sourceWithoutFirstTwo);
