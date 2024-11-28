f49e6adac99e6e9714eb8f2d3907949795ec35d0
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:00:46 +0530

    Update 14_Use_Destructuring_Assignment_to_Pass_an_Object_as_a_Function's_Parameters.js

--- ES6/14_Use_Destructuring_Assignment_to_Pass_an_Object_as_a_Function's_Parameters.js
+++ ES6/14_Use_Destructuring_Assignment_to_Pass_an_Object_as_a_Function's_Parameters.js
@@ -1,3 +1,15 @@
 // Problem 14: Use Destructuring Assignment to Pass an Object as a Function's Parameters
 
-// Write your solution here.
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+
+// Only change code below this line
+const half = ({ max, min }) => (max + min) / 2;
+console.log(half(stats));
+  // Only change code above this line
