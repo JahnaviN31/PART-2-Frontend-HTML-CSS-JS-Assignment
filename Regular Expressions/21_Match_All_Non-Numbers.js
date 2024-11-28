064108540b1ca44293ecf554449a0964de992619
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:53:28 +0530

    Update 21_Match_All_Non-Numbers.js

--- Regular Expressions/21_Match_All_Non-Numbers.js
+++ Regular Expressions/21_Match_All_Non-Numbers.js
@@ -1,3 +1,6 @@
 // Problem 21: Match All Non-Numbers
 
-// Write your solution here.
+let movieName = "2001: A Space Odyssey";
+let noNumRegex = /\D/gi; // Change this line
+let result = movieName.match(noNumRegex).length;
+console.log(result);
