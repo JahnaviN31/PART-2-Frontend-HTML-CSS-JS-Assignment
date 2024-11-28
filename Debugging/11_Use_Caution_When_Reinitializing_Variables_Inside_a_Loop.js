8e22438dbc983fe544578fc8af6d6a27e837ae65
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:13:11 +0530

    Update 11_Use_Caution_When_Reinitializing_Variables_Inside_a_Loop.js

--- Debugging/11_Use_Caution_When_Reinitializing_Variables_Inside_a_Loop.js
+++ Debugging/11_Use_Caution_When_Reinitializing_Variables_Inside_a_Loop.js
@@ -1,3 +1,15 @@
 // Problem 11: Use Caution When Reinitializing Variables Inside a Loop
 
-// Write your solution here.
+function zeroArray(m, n) {
+    let newArray = [];
+    for (let i = 0; i < m; i++) {
+        let row = [];
+        for (let j = 0; j < n; j++) {
+            row.push(0);
+        }
+        newArray.push(row);
+    }
+    return newArray;
+}
+let matrix = zeroArray(3, 2);
+console.log(matrix);
