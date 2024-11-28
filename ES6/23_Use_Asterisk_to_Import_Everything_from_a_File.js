c7d0377808d2240d0776dbd17596491e6aa54655
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:34:04 +0530

    Update 23_Use_Asterisk_to_Import_Everything_from_a_File.js

--- ES6/23_Use_Asterisk_to_Import_Everything_from_a_File.js
+++ ES6/23_Use_Asterisk_to_Import_Everything_from_a_File.js
@@ -1,3 +1,7 @@
 // Problem 23: Use Asterisk to Import Everything from a File
 
-// Write your solution here.
+import * as stringFunctions from "./string_functions.js";
+// Only change code above this line
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
