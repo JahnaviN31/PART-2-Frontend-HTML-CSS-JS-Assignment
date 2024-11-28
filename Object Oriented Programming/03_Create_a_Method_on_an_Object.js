935ecff30429be4b9e12c813f6947e9592e2bf85
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 01:49:35 +0530

    Update 03_Create_a_Method_on_an_Object.js

--- Object Oriented Programming/03_Create_a_Method_on_an_Object.js
+++ Object Oriented Programming/03_Create_a_Method_on_an_Object.js
@@ -1,3 +1,9 @@
 // Problem 03: Create a Method on an Object
 
-// Write your solution here.
+let dog = {
+    name: "Spot",
+    numLegs: 4,
+    sayLegs: function () { return `This dog has ${dog.numLegs} legs.` }
+};
+
+dog.sayLegs();
