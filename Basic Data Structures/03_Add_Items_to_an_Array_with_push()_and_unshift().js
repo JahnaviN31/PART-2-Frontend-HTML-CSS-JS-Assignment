8b0085609f7b647e97bff98a569e87a55b6c1aaf
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:18:19 +0530

    Update 03_Add_Items_to_an_Array_with_push()_and_unshift().js

--- Basic Data Structures/03_Add_Items_to_an_Array_with_push()_and_unshift().js
+++ Basic Data Structures/03_Add_Items_to_an_Array_with_push()_and_unshift().js
@@ -1,3 +1,16 @@
 // Problem 03: Add Items to an Array with push() and unshift()
 
-// Write your solution here.
+function mixedNumbers(arr) {
+    // Only change code below this line
+    arr.unshift(2);
+    arr.unshift("I");
+    arr.unshift("three");
+    
+    arr.push(7);
+    arr.push("VIII");
+    arr.push(9);
+    // Only change code above this line
+    return arr;
+}
+
+console.log(mixedNumbers(['IV', 5, 'six']));
