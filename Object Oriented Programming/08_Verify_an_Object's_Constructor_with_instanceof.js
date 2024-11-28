ccf25abe189219b40506962a43b076422222830f
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 01:53:35 +0530

    Update 08_Verify_an_Object's_Constructor_with_instanceof.js

--- Object Oriented Programming/08_Verify_an_Object's_Constructor_with_instanceof.js
+++ Object Oriented Programming/08_Verify_an_Object's_Constructor_with_instanceof.js
@@ -1,3 +1,10 @@
 // Problem 08: Verify an Object's Constructor with instanceof
 
-// Write your solution here.
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+}
+
+// Only change code below this line
+let myHouse = new House(3);
+console.log(myHouse instanceof House);
+// Only change code below this line
