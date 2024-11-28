c2a2b382f510935996cd8a6234fb36e4e381a868
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 11:45:31 +0530

    Update 03_Prevent_Object_Mutation.js

--- ES6/03_Prevent_Object_Mutation.js
+++ ES6/03_Prevent_Object_Mutation.js
@@ -1,8 +1,5 @@
 // Problem 03: Prevent Object Mutation
 
-// In this challenge you are going to use Object.freeze to prevent mathematical constants from changing. 
-// You need to freeze the MATH_CONSTANTS object so that no one is able to alter the value of PI, add, or delete properties.
-
 function freezeObj() {
     const MATH_CONSTANTS = {
         PI: 3.14
