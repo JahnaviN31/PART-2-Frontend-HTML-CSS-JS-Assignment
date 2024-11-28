c0ff665a1774fca66f40c9e47b69ce19341122b0
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:16:55 +0530

    Update 02_Access_an_Array's_Contents_Using_Bracket_Notation.js

--- Basic Data Structures/02_Access_an_Array's_Contents_Using_Bracket_Notation.js
+++ Basic Data Structures/02_Access_an_Array's_Contents_Using_Bracket_Notation.js
@@ -1,3 +1,7 @@
 // Problem 02: Access an Array's Contents Using Bracket Notation
 
-// Write your solution here.
+let myArray = ["a", "b", "c", "d"];
+// Only change code below this line
+myArray[1] = "_b";
+// Only change code above this line
+console.log(myArray);
