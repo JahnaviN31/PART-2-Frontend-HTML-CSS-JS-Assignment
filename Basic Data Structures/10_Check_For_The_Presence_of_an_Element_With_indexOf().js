183b2ca402c3bcf269e0aa5ee6a488b674124157
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:23:37 +0530

    Update 10_Check_For_The_Presence_of_an_Element_With_indexOf().js

--- Basic Data Structures/10_Check_For_The_Presence_of_an_Element_With_indexOf().js
+++ Basic Data Structures/10_Check_For_The_Presence_of_an_Element_With_indexOf().js
@@ -1,3 +1,10 @@
 // Problem 10: Check For The Presence of an Element With indexOf()
 
-// Write your solution here.
+function quickCheck(arr, elem) {
+    // Only change code below this line
+    const response = arr.indexOf(elem) !== -1;
+    return response;
+    // Only change code above this line
+}
+
+console.log(quickCheck([3, 5, 9, 125, 45, 2], 125));
