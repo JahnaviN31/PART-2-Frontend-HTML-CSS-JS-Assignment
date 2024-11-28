a644e8ed8b9d1bb712c432975dcf20dc6b89161a
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:36:36 +0530

    Update 18_Iterate_Through_the_Keys_of_an_Object_with_a_for...in_Statement.js

--- Basic Data Structures/18_Iterate_Through_the_Keys_of_an_Object_with_a_for...in_Statement.js
+++ Basic Data Structures/18_Iterate_Through_the_Keys_of_an_Object_with_a_for...in_Statement.js
@@ -1,3 +1,24 @@
 // Problem 18: Iterate Through the Keys of an Object with a for...in Statement
 
-// Write your solution here.
+const users = {
+    Alan: {
+        online: false
+    },
+    Jeff: {
+        online: true
+    },
+    Sarah: {
+        online: false
+    }
+}
+
+function countOnline(usersObj) {
+    // Only change code below this line
+    let count = 0;
+    for (let user in usersObj) {
+        usersObj[user].online === true && count++;
+    }
+    return count;
+    // Only change code above this line
+}
+console.log(countOnline(users));
