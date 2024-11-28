88bd6aac1d7ac1546030cac1641a3c9841038f44
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:44:00 +0530

    Update 03_Factorialize_a_Number.js

--- Basic Algorithm Scripting/03_Factorialize_a_Number.js
+++ Basic Algorithm Scripting/03_Factorialize_a_Number.js
@@ -1,3 +1,7 @@
 // Problem 03: Factorialize a Number
 
-// Write your solution here.
+function factorialize(num) {
+    if (num === 0) return 1;
+    return num * factorialize(num - 1);
+}
+console.log(factorialize(5));
