7c9a438869e62f1e4c68ef0ee4015d1cc3b7bdc3
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 11:56:42 +0530

    Update 11_Use_Destructuring_Assignment_to_Assign_Variables_from_Nested_Objects.js

--- ES6/11_Use_Destructuring_Assignment_to_Assign_Variables_from_Nested_Objects.js
+++ ES6/11_Use_Destructuring_Assignment_to_Assign_Variables_from_Nested_Objects.js
@@ -1,3 +1,13 @@
 // Problem 11: Use Destructuring Assignment to Assign Variables from Nested Objects
 
-// Write your solution here.
+const LOCAL_FORECAST = {
+    yesterday: { low: 61, high: 75 },
+    today: { low: 64, high: 77 },
+    tomorrow: { low: 68, high: 80 }
+};
+
+// Only change code below this line
+const { today: { low: lowToday } } = LOCAL_FORECAST;
+const { today: { high: highToday } } = LOCAL_FORECAST;
+console.log(lowToday,highToday);
+  // Only change code above this line
