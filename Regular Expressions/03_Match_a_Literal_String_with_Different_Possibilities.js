9649d7e3f8a63c4d048d617f841754a754369b67
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:42:42 +0530

    Update 03_Match_a_Literal_String_with_Different_Possibilities.js

--- Regular Expressions/03_Match_a_Literal_String_with_Different_Possibilities.js
+++ Regular Expressions/03_Match_a_Literal_String_with_Different_Possibilities.js
@@ -1,3 +1,5 @@
 // Problem 03: Match a Literal String with Different Possibilities
 
-// Write your solution here.
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
