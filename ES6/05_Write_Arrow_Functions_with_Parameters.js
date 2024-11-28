2a5d14f51ae40194374983abaffe4dccdffaf76d
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 11:38:56 +0530

    Update 05_Write_Arrow_Functions_with_Parameters.js

--- ES6/05_Write_Arrow_Functions_with_Parameters.js
+++ ES6/05_Write_Arrow_Functions_with_Parameters.js
@@ -1,3 +1,9 @@
 // Problem 05: Write Arrow Functions with Parameters
 
-// Write your solution here.
+// Rewrite the myConcat function which appends contents of arr2 to arr1 so that the function uses arrow function syntax.
+
+const myConcat = (arr1, arr2) => {
+    return arr1.concat(arr2);
+};
+
+console.log(myConcat([1, 2], [3, 4, 5]));
