1f45dfee751d91fe3d478ddbc13f1c36d8f47c34
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:49:38 +0530

    Update 14_Find_Characters_with_Lazy_Matching.js

--- Regular Expressions/14_Find_Characters_with_Lazy_Matching.js
+++ Regular Expressions/14_Find_Characters_with_Lazy_Matching.js
@@ -1,3 +1,6 @@
 // Problem 14: Find Characters with Lazy Matching
 
-// Write your solution here.
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<h[1]*?>/; // Change this line
+let result = text.match(myRegex);
+console.log(result);
