4824632493fbe3b1853d5319a1d02f50f0647191
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 00:47:16 +0530

    Update 11_Title_Case_a_Sentence.js

--- Basic Algorithm Scripting/11_Title_Case_a_Sentence.js
+++ Basic Algorithm Scripting/11_Title_Case_a_Sentence.js
@@ -1,3 +1,13 @@
 // Problem 11: Title Case a Sentence
 
-// Write your solution here.
+function titleCase(str) {
+
+    const newTitle = str.split(" ");
+    const result = [];
+    for (let st in newTitle) {
+        result[st] = newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+    }
+    return result.join(" ");
+}
+
+console.log(titleCase("I'm a little tea pot"));
