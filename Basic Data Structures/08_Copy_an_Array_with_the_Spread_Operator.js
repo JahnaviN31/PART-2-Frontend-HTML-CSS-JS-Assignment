abbf7b0f01a7969a0da4469f6968c8e0b7e09fc2
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 13:22:17 +0530

    Update 08_Copy_an_Array_with_the_Spread_Operator.js

--- Basic Data Structures/08_Copy_an_Array_with_the_Spread_Operator.js
+++ Basic Data Structures/08_Copy_an_Array_with_the_Spread_Operator.js
@@ -1,3 +1,15 @@
 // Problem 08: Copy an Array with the Spread Operator
 
-// Write your solution here.
+function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+        // change code below this line
+        newArr.push([...arr]);
+        // change code above this line
+        num--;
+    }
+    return newArr;
+}
+
+// change code here to test different cases:
+console.log(copyMachine([true, false, true], 2));
