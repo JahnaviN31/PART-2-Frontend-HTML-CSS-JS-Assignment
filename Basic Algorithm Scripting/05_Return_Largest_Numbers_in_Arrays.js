36ccff1aa5846b0804ab682b5bb5f15b120392c6
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 00:14:53 +0530

    Update 05_Return_Largest_Numbers_in_Arrays.js

--- Basic Algorithm Scripting/05_Return_Largest_Numbers_in_Arrays.js
+++ Basic Algorithm Scripting/05_Return_Largest_Numbers_in_Arrays.js
@@ -1,3 +1,15 @@
 // Problem 05: Return Largest Numbers in Arrays
 
-// Write your solution here.
+function largestOfFour(arr) {
+    const max = [];
+    for (let i = 0; i < arr.length; i++) {
+        let largest = arr[i][0];
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] > largest) largest = arr[i][j];
+        }
+        max[i] = largest;
+    }
+    return max;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
