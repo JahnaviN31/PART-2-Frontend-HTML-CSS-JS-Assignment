e3f82293a8e053bd41fb091a79847bdc09ff7a62
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:45:26 +0530

    Update 07_Match_Anything_with_Wildcard_Period.js

--- Regular Expressions/07_Match_Anything_with_Wildcard_Period.js
+++ Regular Expressions/07_Match_Anything_with_Wildcard_Period.js
@@ -1,3 +1,5 @@
 // Problem 07: Match Anything with Wildcard Period
 
-// Write your solution here.
+let exampleStr = "Let's have fun with regular expressions!";
+let unRegex = /ru.|su.|fu.|pu.|nu.|bun/; // Change this line
+let result = unRegex.test(exampleStr);
