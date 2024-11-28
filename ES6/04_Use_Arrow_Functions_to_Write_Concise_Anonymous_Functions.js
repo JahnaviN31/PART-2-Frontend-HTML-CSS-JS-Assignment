7062b2c188f5cd69fadb790a5dc65202f43ad556
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 11:38:16 +0530

    Update 04_Use_Arrow_Functions_to_Write_Concise_Anonymous_Functions.js

--- ES6/04_Use_Arrow_Functions_to_Write_Concise_Anonymous_Functions.js
+++ ES6/04_Use_Arrow_Functions_to_Write_Concise_Anonymous_Functions.js
@@ -1,3 +1,8 @@
 // Problem 04: Use Arrow Functions to Write Concise Anonymous Functions
 
-// Write your solution here.
+// Rewrite the function assigned to the variable magic which returns a new Date() to use arrow function syntax. 
+// Also, make sure nothing is defined using the keyword var.
+
+const magic = () => {
+    return new Date();
+};
