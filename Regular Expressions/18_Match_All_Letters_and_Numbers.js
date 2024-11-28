0429fa7f60ea7ab316e6aade7fc9c1eaf24caf3a
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:51:49 +0530

    Update 18_Match_All_Letters_and_Numbers.js

--- Regular Expressions/18_Match_All_Letters_and_Numbers.js
+++ Regular Expressions/18_Match_All_Letters_and_Numbers.js
@@ -1,3 +1,7 @@
 // Problem 18: Match All Letters and Numbers
 
-// Write your solution here.
+let quoteSample = "The five boxing wizards jump quickly.";
+let alphabetRegexV2 = /\w/gi; // Change this line
+let result = quoteSample.match(alphabetRegexV2).length;
+
+console.log(result);
