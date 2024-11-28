36bb58dc95c80b9ed3adbd0500bcbb8bfcb93836
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 02:01:47 +0530

    Update 16_Understand_the_Prototype_Chain.js

--- Object Oriented Programming/16_Understand_the_Prototype_Chain.js
+++ Object Oriented Programming/16_Understand_the_Prototype_Chain.js
@@ -1,3 +1,12 @@
 // Problem 16: Understand the Prototype Chain
 
-// Write your solution here.
+function Dog(name) {
+    this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+
+// Fix the code below so that it evaluates to true
+console.log(Object.prototype.isPrototypeOf(Dog.prototype));
