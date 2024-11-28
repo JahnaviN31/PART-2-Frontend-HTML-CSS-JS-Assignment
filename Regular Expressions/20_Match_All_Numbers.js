d77fb03b4db4e5ac798c4345ab817f87809c26d8
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:52:59 +0530

    Update 20_Match_All_Numbers.js

--- Regular Expressions/20_Match_All_Numbers.js
+++ Regular Expressions/20_Match_All_Numbers.js
@@ -1,3 +1,6 @@
 // Problem 20: Match All Numbers
 
-// Write your solution here.
+let movieName = "2001: A Space Odyssey";
+let numRegex = /\d/g; // Change this line
+let result = movieName.match(numRegex).length;
+console.log(result);
