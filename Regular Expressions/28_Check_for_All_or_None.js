0deb151ac622fd52846efb90261386bbbef1ae40
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:57:00 +0530

    Update 28_Check_for_All_or_None.js

--- Regular Expressions/28_Check_for_All_or_None.js
+++ Regular Expressions/28_Check_for_All_or_None.js
@@ -1,3 +1,7 @@
 // Problem 28: Check for All or None
 
-// Write your solution here.
+let favWord = "favorite";
+let favRegex = /favou?rite/; // Change this line
+let result = favRegex.test(favWord);
+
+console.log(result);
