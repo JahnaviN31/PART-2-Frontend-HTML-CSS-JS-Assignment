cf62f83098e75122a1c563d37fbffe45dac62c6c
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 00:38:09 +0530

    Update 09_Finders_Keepers.js

--- Basic Algorithm Scripting/09_Finders_Keepers.js
+++ Basic Algorithm Scripting/09_Finders_Keepers.js
@@ -1,3 +1,12 @@
 // Problem 09: Finders Keepers
 
-// Write your solution here.
+function findElement(arr, func) {
+    let num = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            return arr[i];
+        }
+    }
+    return;
+}
+console.log(findElement([1, 3, 5, 8, 9, 10], num => num % 2 === 0));
