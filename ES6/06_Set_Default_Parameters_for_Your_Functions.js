cad449426572d794c219dd4d375b8dc33e3e65a2
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 11:40:25 +0530

    Update 06_Set_Default_Parameters_for_Your_Functions.js

--- ES6/06_Set_Default_Parameters_for_Your_Functions.js
+++ ES6/06_Set_Default_Parameters_for_Your_Functions.js
@@ -1,3 +1,8 @@
 // Problem 06: Set Default Parameters for Your Functions
 
-// Write your solution here.
+// Modify the function increment by adding default parameters so that it will add 1 to number if value is not specified.
+
+// Only change code below this line
+const increment = (number,value = 1) => number + value;
+console.log(increment(5,2));
+// Only change code above this line
