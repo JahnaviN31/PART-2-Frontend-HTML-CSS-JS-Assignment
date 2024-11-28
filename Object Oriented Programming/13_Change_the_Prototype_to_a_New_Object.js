bfd570214d2e5d97c182de989a0fc1fe955d33a3
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 01:59:34 +0530

    Update 13_Change_the_Prototype_to_a_New_Object.js

--- Object Oriented Programming/13_Change_the_Prototype_to_a_New_Object.js
+++ Object Oriented Programming/13_Change_the_Prototype_to_a_New_Object.js
@@ -1,3 +1,12 @@
 // Problem 13: Change the Prototype to a New Object
 
-// Write your solution here.
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype = {
+    // Only change code below this line
+    numLegs: 4,
+    eat: function () { },
+    describe: function () { }
+};
