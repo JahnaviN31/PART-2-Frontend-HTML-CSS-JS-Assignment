08fd6ca3148ee6d818956d90ab90c7f528564a27
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:59:30 +0530

    Update 32_Use_Capture_Groups_to_Search_and_Replace.js

--- Regular Expressions/32_Use_Capture_Groups_to_Search_and_Replace.js
+++ Regular Expressions/32_Use_Capture_Groups_to_Search_and_Replace.js
@@ -1,3 +1,8 @@
 // Problem 32: Use Capture Groups to Search and Replace
 
-// Write your solution here.
+let str = "one two three";
+let fixRegex = /(\w+)\s(\w+)\s(\w+)/; // Change this line
+let replaceText = "$3 $2 $1"; // Change this line
+let result = str.replace(fixRegex, replaceText);
+
+console.log(result);
