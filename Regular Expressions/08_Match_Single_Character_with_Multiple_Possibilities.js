283e8bed64991db20c7e9806f659e792c4345eee
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:46:00 +0530

    Update 08_Match_Single_Character_with_Multiple_Possibilities.js

--- Regular Expressions/08_Match_Single_Character_with_Multiple_Possibilities.js
+++ Regular Expressions/08_Match_Single_Character_with_Multiple_Possibilities.js
@@ -1,3 +1,6 @@
 // Problem 08: Match Single Character with Multiple Possibilities
 
-// Write your solution here.
+let quoteSample = "Beware of bugs in the above code; I have only proved it correct, not tried it.";
+let vowelRegex = /[aeiou]/gi; // Change this line
+let result = quoteSample.match(vowelRegex); // Change this line
+console.log(result);
