92025c78a6fc0a4a27b62a54cd1b774aaf03a21e
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 00:21:26 +0530

    Update 07_Repeat_a_String_Repeat_a_String.js

--- Basic Algorithm Scripting/07_Repeat_a_String_Repeat_a_String.js
+++ Basic Algorithm Scripting/07_Repeat_a_String_Repeat_a_String.js
@@ -1,3 +1,14 @@
 // Problem 07: Repeat a String Repeat a String
 
-// Write your solution here.
+function repeatStringNumTimes(str, num) {
+    if (num <= 0) {
+        return ""; 
+    }
+    let result = ""; 
+    for (let i = 0; i < num; i++) {
+        result += str; 
+    }
+    return result; 
+}
+
+repeatStringNumTimes("abc", 3);
