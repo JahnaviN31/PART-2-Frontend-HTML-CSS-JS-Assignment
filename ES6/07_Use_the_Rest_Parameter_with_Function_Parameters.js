9f30f0520cb824e279256e3f79af8f6978136485
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 11:42:13 +0530

    Update 07_Use_the_Rest_Parameter_with_Function_Parameters.js

--- ES6/07_Use_the_Rest_Parameter_with_Function_Parameters.js
+++ ES6/07_Use_the_Rest_Parameter_with_Function_Parameters.js
@@ -1,3 +1,13 @@
 // Problem 07: Use the Rest Parameter with Function Parameters
 
-// Write your solution here.
+// Modify the function sum using the rest parameter in such a way that the function sum is able to take any number of arguments and return their sum.
+
+const sum = (...args) => {
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+        total += args[i];
+    }
+    return total;
+}
+
+console.log(sum());
