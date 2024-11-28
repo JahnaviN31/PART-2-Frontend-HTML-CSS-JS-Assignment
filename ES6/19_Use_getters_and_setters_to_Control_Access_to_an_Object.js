0efba5f7734a3bfa2a0358911025800f0182f682
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-28 12:30:19 +0530

    Update 19_Use_getters_and_setters_to_Control_Access_to_an_Object.js

--- ES6/19_Use_getters_and_setters_to_Control_Access_to_an_Object.js
+++ ES6/19_Use_getters_and_setters_to_Control_Access_to_an_Object.js
@@ -1,3 +1,20 @@
 // Problem 19: Use getters and setters to Control Access to an Object
 
-// Write your solution here.
+class Thermostat {
+    constructor(fahrenheit) {
+        this.celsius = ((fahrenheit - 32)*5) / 9;
+    }
+    get temperature() {
+        return this.celsius;
+    }
+    set temperature(newTemperature) {
+        this.celsius = newTemperature;
+    }
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+console.log(temp);
