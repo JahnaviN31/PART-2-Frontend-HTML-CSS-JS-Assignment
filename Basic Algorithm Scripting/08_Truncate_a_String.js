7c3e247970d147827b1bbd92f50c4124e04ea2aa
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 00:22:51 +0530

    Update 08_Truncate_a_String.js

--- Basic Algorithm Scripting/08_Truncate_a_String.js
+++ Basic Algorithm Scripting/08_Truncate_a_String.js
@@ -1,3 +1,11 @@
 // Problem 08: Truncate a String
 
-// Write your solution here.
+function truncateString(str, num) {
+    if (str.length > num) {
+        return str.slice(0, num) + "...";
+    } else {
+        return str;
+    }
+}
+
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length));
