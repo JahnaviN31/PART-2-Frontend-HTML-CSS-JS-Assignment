a8eac70b01969fe1b93ceed84cb2912881f02dfc
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 10:03:27 +0530

    Update 19_Set_the_Child's_Prototype_to_an_Instance_of_the_Parent.js

--- Object Oriented Programming/19_Set_the_Child's_Prototype_to_an_Instance_of_the_Parent.js
+++ Object Oriented Programming/19_Set_the_Child's_Prototype_to_an_Instance_of_the_Parent.js
@@ -1,3 +1,16 @@
 // Problem 19: Set the Child's Prototype to an Instance of the Parent
 
-// Write your solution here.
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function () {
+        console.log("nom nom nom");
+    }
+};
+
+function Dog() { }
+
+// Only change code below this line
+Dog.prototype = Object.create(Animal.prototype);
+let beagle = new Dog();
