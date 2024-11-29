b9a612c9fc3cef02ad8518b695699c19845d02d9
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 10:01:44 +0530

    Update 17_Use_Inheritance_So_You_Don't_Repeat_Yourself.js

--- Object Oriented Programming/17_Use_Inheritance_So_You_Don't_Repeat_Yourself.js
+++ Object Oriented Programming/17_Use_Inheritance_So_You_Don't_Repeat_Yourself.js
@@ -1,3 +1,26 @@
 // Problem 17: Use Inheritance So You Don't Repeat Yourself
 
-// Write your solution here.
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype = {
+  constructor: Cat,
+};
+
+function Bear(name) {
+  this.name = name;
+}
+
+Bear.prototype = {
+  constructor: Bear,
+};
+
+function Animal() {}
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
