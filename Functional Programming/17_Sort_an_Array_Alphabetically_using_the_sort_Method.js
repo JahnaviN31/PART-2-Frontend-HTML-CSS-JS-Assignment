15944abc2cb34c7ae03a30169d46337089b869f9
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 12:19:50 +0530

    Update 17_Sort_an_Array_Alphabetically_using_the_sort_Method.js

--- Functional Programming/17_Sort_an_Array_Alphabetically_using_the_sort_Method.js
+++ Functional Programming/17_Sort_an_Array_Alphabetically_using_the_sort_Method.js
@@ -1,3 +1,11 @@
 // Problem 17: Sort an Array Alphabetically using the sort Method
 
-// Write your solution here.
+function alphabeticalOrder(arr) {
+    // Only change code below this line
+    return arr.sort((a,b) => {
+        return a === b ? 0 : a < b ? -1 : 1;
+    });
+    // Only change code above this line
+}
+
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
