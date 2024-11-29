c4c18d2f3a8437c7df556f2435013e188549709f
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 12:21:55 +0530

    Update 18_Return_a_Sorted_Array_Without_Changing_the_Original_Array.js

--- Functional Programming/18_Return_a_Sorted_Array_Without_Changing_the_Original_Array.js
+++ Functional Programming/18_Return_a_Sorted_Array_Without_Changing_the_Original_Array.js
@@ -1,3 +1,11 @@
 // Problem 18: Return a Sorted Array Without Changing the Original Array
 
-// Write your solution here.
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+  return arr.slice().sort((a, b) => a - b);
+  // Only change code above this line
+}
+
+nonMutatingSort(globalArray);
