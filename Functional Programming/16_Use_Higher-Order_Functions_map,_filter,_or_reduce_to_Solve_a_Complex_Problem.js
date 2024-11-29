e13847444e32b0811fc7d605e65ea194e33f11da
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 12:15:54 +0530

    Update 16_Use_Higher-Order_Functions_map,_filter,_or_reduce_to_Solve_a_Complex_Problem.js

--- Functional Programming/16_Use_Higher-Order_Functions_map,_filter,_or_reduce_to_Solve_a_Complex_Problem.js
+++ Functional Programming/16_Use_Higher-Order_Functions_map,_filter,_or_reduce_to_Solve_a_Complex_Problem.js
@@ -1,3 +1,12 @@
 // Problem 16: Use Higher-Order Functions map, filter, or reduce to Solve a Complex Problem
 
-// Write your solution here.
+const squareList = arr => {
+  // Only change code below this line
+  return arr
+    .filter(num => num > 0 && Number.isInteger(num)) 
+    .map(num => num ** 2); 
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
