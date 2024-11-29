4904b805af15bb22225a637d4e999e47e1a1c80f
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 12:23:04 +0530

    Update 19_Split_a_String_into_an_Array_Using_the_split_Method.js

--- Functional Programming/19_Split_a_String_into_an_Array_Using_the_split_Method.js
+++ Functional Programming/19_Split_a_String_into_an_Array_Using_the_split_Method.js
@@ -1,3 +1,9 @@
 // Problem 19: Split a String into an Array Using the split Method
 
-// Write your solution here.
+function splitify(str) {
+  // Only change code below this line
+  return str.split(/\W/); 
+  // Only change code above this line
+}
+
+splitify("Hello World,I-am code");
