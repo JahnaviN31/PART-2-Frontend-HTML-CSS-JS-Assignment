4ad720b7662c5382306d460725fb2151d495c824
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 11:45:42 +0530

    Update 10_Implement_the_filter_Method_on_a_Prototype.js

--- Functional Programming/10_Implement_the_filter_Method_on_a_Prototype.js
+++ Functional Programming/10_Implement_the_filter_Method_on_a_Prototype.js
@@ -1,3 +1,13 @@
 // Problem 10: Implement the filter Method on a Prototype
 
-// Write your solution here.
+Array.prototype.myFilter = function (callback) {
+    const newArray = [];
+    // Only change code below this line
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this) == true) {
+            newArray.push(this[i]);
+        }
+    }
+    // Only change code above this line
+    return newArray;
+};
