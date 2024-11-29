2b8db6a7c740438f5029962802f6ef0a7d49dc2b
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 10:09:04 +0530

    Update 23_Use_a_Mixin_to_Add_Common_Behavior_Between_Unrelated_Objects.js

--- Object Oriented Programming/23_Use_a_Mixin_to_Add_Common_Behavior_Between_Unrelated_Objects.js
+++ Object Oriented Programming/23_Use_a_Mixin_to_Add_Common_Behavior_Between_Unrelated_Objects.js
@@ -1,3 +1,20 @@
 // Problem 23: Use a Mixin to Add Common Behavior Between Unrelated Objects
 
-// Write your solution here.
+let bird = {
+    name: "Donald",
+    numLegs: 2
+};
+
+let boat = {
+    name: "Warrior",
+    type: "race-boat"
+};
+
+// Only change code below this line
+
+let glideMixin = function (obj) {
+    obj.glide = function () { }
+}
+
+glideMixin(bird);
+glideMixin(boat);
