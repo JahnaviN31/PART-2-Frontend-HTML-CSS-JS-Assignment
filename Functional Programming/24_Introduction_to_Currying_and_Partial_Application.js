c40e7305b7db3c9f98c9e16d8af83aa6029ee351
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 12:34:37 +0530

    Update 24_Introduction_to_Currying_and_Partial_Application.js

--- Functional Programming/24_Introduction_to_Currying_and_Partial_Application.js
+++ Functional Programming/24_Introduction_to_Currying_and_Partial_Application.js
@@ -1,3 +1,12 @@
 // Problem 24: Introduction to Currying and Partial Application
 
-// Write your solution here.
+function add(x) {
+  // Only change code below this line
+  return function add(y) {
+        return function add(z) {
+            return x + y + z;
+        }
+    }
+  // Only change code above this line
+}
+add(10)(20)(30);
