084c837ed291079b8e6e8b6778a8672b3873dd34
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 12:32:03 +0530

    Update 22_Use_the_every_Method_to_Check_that_Every_Element_in_an_Array_Meets_a_Criteria.js

--- Functional Programming/22_Use_the_every_Method_to_Check_that_Every_Element_in_an_Array_Meets_a_Criteria.js
+++ Functional Programming/22_Use_the_every_Method_to_Check_that_Every_Element_in_an_Array_Meets_a_Criteria.js
@@ -1,3 +1,11 @@
 // Problem 22: Use the every Method to Check that Every Element in an Array Meets a Criteria
 
-// Write your solution here.
+function checkPositive(arr) {
+  // Only change code below this line
+  return arr.every((item) => {
+        return item > 0;
+    })
+  // Only change code above this line
+}
+
+checkPositive([1, 2, 3, -4, 5]);
