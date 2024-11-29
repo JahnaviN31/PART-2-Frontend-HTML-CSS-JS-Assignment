8e4bcb1c1a2926d2ff6b60687315b1bfc96b5d67
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 12:29:05 +0530

    Update 21_Apply_Functional_Programming_to_Convert_Strings_to_URL_Slugs.js

--- Functional Programming/21_Apply_Functional_Programming_to_Convert_Strings_to_URL_Slugs.js
+++ Functional Programming/21_Apply_Functional_Programming_to_Convert_Strings_to_URL_Slugs.js
@@ -1,3 +1,8 @@
 // Problem 21: Apply Functional Programming to Convert Strings to URL Slugs
 
-// Write your solution here.
+// Only change code below this line
+function urlSlug(title) {
+  return title.trim().toLowerCase().split(/\s+/).join('-');
+}
+// Only change code above this line
+urlSlug("A Mind Needs Books Like A Sword Needs A Whetstone");
