98a0bfdf8af172f24b69aa7e821ffe1a66b563b6
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 11:46:21 +0530

    Update 11_Return_Part_of_an_Array_Using_the_slice_Method.js

--- Functional Programming/11_Return_Part_of_an_Array_Using_the_slice_Method.js
+++ Functional Programming/11_Return_Part_of_an_Array_Using_the_slice_Method.js
@@ -1,3 +1,10 @@
 // Problem 11: Return Part of an Array Using the slice Method
 
-// Write your solution here.
+function sliceArray(anim, beginSlice, endSlice) {
+    // Only change code below this line
+    return anim.slice(beginSlice, endSlice);
+    // Only change code above this line
+}
+
+const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+console.log(sliceArray(inputAnim, 1, 3));
