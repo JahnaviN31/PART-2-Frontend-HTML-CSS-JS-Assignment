730b6ff8cb6487a5789fa7042d0b649f8a9d0be8
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 11:30:00 +0530

    Update 06_Refactor_Global_Variables_Out_of_Functions.js

--- Functional Programming/06_Refactor_Global_Variables_Out_of_Functions.js
+++ Functional Programming/06_Refactor_Global_Variables_Out_of_Functions.js
@@ -1,3 +1,28 @@
 // Problem 06: Refactor Global Variables Out of Functions
 
-// Write your solution here.
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(book, bookName) {
+
+    let newBookList = [...book];
+    newBookList.push(bookName);
+    return newBookList;
+
+    // Change code above this line
+}
+
+// Change code below this line
+function remove(book, bookName) {
+
+    let newBookList = [...book];
+    const book_index = bookList.indexOf(bookName);
+    if (book_index >= 0) {
+
+        newBookList.splice(book_index, 1);
+        return newBookList;
+
+        // Change code above this line
+    }
+}
