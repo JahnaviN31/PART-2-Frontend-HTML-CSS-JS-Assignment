ba78aebfd3880ab0f5ed9024bca1c3660e5097d8
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 10:07:57 +0530

    Update 22_Override_Inherited_Methods.js

--- Object Oriented Programming/22_Override_Inherited_Methods.js
+++ Object Oriented Programming/22_Override_Inherited_Methods.js
@@ -1,3 +1,21 @@
 // Problem 22: Override Inherited Methods
 
-// Write your solution here.
+function Bird() { }
+
+Bird.prototype.fly = function () { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+// Only change code below this line
+Penguin.prototype.fly = function () {
+    return ("Alas, this is a flightless bird.");
+}
+// Only change code above this line
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+
+let bird = new Bird();
+console.log(bird.fly("I am flying"));
