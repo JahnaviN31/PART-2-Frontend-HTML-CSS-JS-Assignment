54029ffe6b1cf7f4b0af67e0be073a34e9c1a6e8
Jahnavi <149796546+JahnaviN31@users.noreply.github.com>
2024-11-29 12:24:46 +0530

    Update 20_Combine_an_Array_into_a_String_Using_the_join_Method.js

--- Functional Programming/20_Combine_an_Array_into_a_String_Using_the_join_Method.js
+++ Functional Programming/20_Combine_an_Array_into_a_String_Using_the_join_Method.js
@@ -1,3 +1,9 @@
 // Problem 20: Combine an Array into a String Using the join Method
 
-// Write your solution here.
+function sentensify(str) {
+  // Only change code below this line
+  return str.split(/\W/).join(" ");
+  // Only change code above this line
+}
+
+sentensify("May-the-force-be-with-you");
